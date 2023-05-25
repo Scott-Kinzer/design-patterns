b8771bf0e03fe6324120a3e91144b70f7e49d6f4
Sasha <zaz765831@gmail.com>
2023-05-25 16:23:19 +0300

    observer

---
+++ pattern/observer.ts
@@ -0,0 +1,40 @@
+interface Observer {
+    update(data: any): void;
+  }
+  
+  class Subject {
+    private observers: Observer[] = [];
+  
+    public addObserver(observer: Observer): void {
+      this.observers.push(observer);
+    }
+  
+    public removeObserver(observer: Observer): void {
+      const index = this.observers.indexOf(observer);
+      if (index > -1) {
+        this.observers.splice(index, 1);
+      }
+    }
+  
+    public notifyObservers(data: any): void {
+      this.observers.forEach((observer) => {
+        observer.update(data);
+      });
+    }
+  }
+  
+  class ConcreteObserver implements Observer {
+    public update(data: any): void {
+      console.log('Received new data:', data);
+    }
+  }
+  
+  const subject = new Subject();
+  const observer1 = new ConcreteObserver();
+  const observer2 = new ConcreteObserver();
+  
+  subject.addObserver(observer1);
+  subject.addObserver(observer2);
+  
+  subject.notifyObservers('New data');
+  
